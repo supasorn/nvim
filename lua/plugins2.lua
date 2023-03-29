b8e82a2db38bdce8d980930aabf240002fce099d
Supasorn Suwajanakorn <supasorn@gmail.com>
2023-03-29 14:28:08 +0700

    auto commit

--- lua/plugins2.lua
+++ lua/plugins2.lua
@@ -1308,4 +1308,15 @@ return {
     cmd = {'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles', 'DiffviewRefresh'},
 
   },
+  { 'github/copilot.vim',
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+      vim.cmd [[
+        imap <silent><script><expr> <c-n> copilot#Accept("\<CR>")
+      ]]
+    end,
+  },
+
 }
