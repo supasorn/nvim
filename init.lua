16f2e42d9d349497d2e92820d9bb696af1a51dc2
supasorn <supasorn@gmail.com>
2023-12-17 15:22:35 +0700

    auto commit

--- init.lua
+++ init.lua
@@ -174,9 +174,11 @@ vnoremap // y/\V<c-r>=escape(@",'/\')<cr><cr>
 nnoremap gy /\V<c-r>=escape(@",'/\')<cr><cr>
 
 " command! -nargs=+ H lua LaunchHttpServerHere(<f-args>)
-" command! Vscode execute '!' . "ssh $(echo $SSH_CLIENT | awk '\{ print $1 \}') '/usr/local/bin/code --folder-uri \"vscode-remote://ssh-remote+$(hostname -I | awk '\{print $1\}')$(pwd)\"'"
 command! H lua LaunchHttpServerPwd()
-command! Vscode execute '!' . "ssh $(echo $SSH_CLIENT | awk '\{ print $1 \}') '/usr/local/bin/code --folder-uri \"vscode-remote://ssh-remote+'$(hostname -I | awk '{print $1}')''$(pwd)'\"'"
+
+" command! Vscode execute '!' . "ssh $(echo $SSH_CLIENT | awk '\{ print $1 \}') '/usr/local/bin/code --folder-uri \"vscode-remote://ssh-remote+'$(hostname -I | awk '{print $1}')''$(pwd)'\"'"
+command! Vscode execute '!' . "ssh -o StrictHostKeyChecking=no $(if [ -s ~/ssh_client_info.txt ]; then cat ~/ssh_client_info.txt | awk '\{ print $1 \}'; else echo $SSH_CLIENT | awk '\{ print $1 \}'; fi) '/usr/local/bin/code --folder-uri \"vscode-remote://ssh-remote+'$(hostname -I | awk '{print $1}')''$(pwd)'\"'"
+
 
 
 
