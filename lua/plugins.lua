a2bb18fb5ae3ca79ecbe3cd957d545715af683e4
Supasorn Suwajanakorn <supasorn@gmail.com>
2024-09-19 11:59:28 +0700

    auto commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -806,10 +806,14 @@ return {
             -- },
             --
             {
-              'lsp_progress',
-              display_components = { { 'title', 'percentage', 'message' } },
-              spinner_symbols = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏', },
-              separator = {},
+              -- 'lsp_progress',
+              -- display_components = { { 'title', 'percentage', 'message' } },
+              -- spinner_symbols = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏', },
+              -- separator = {},
+              function()
+                -- invoke `progress` here.
+                return require('lsp-progress').progress()
+              end,
             },
             {
               'diagnostics',
@@ -969,8 +973,62 @@ return {
       vim.notify = notify
     end
   },
-  { 'WhoIsSethDaniel/lualine-lsp-progress.nvim',
-    lazy = true,
+  { 'linrongbin16/lsp-progress.nvim',
+    -- lazy = true,
+    config = function()
+      require('lsp-progress').setup {
+        client_format = function(client_name, spinner, series_messages)
+          if #series_messages == 0 then
+            return nil
+          end
+          return {
+            name = client_name,
+            body = spinner .. " " .. table.concat(series_messages, ", "),
+          }
+        end,
+        format = function(client_messages)
+          --- @param name string
+          --- @param msg string?
+          --- @return string
+          local function stringify(name, msg)
+            return msg and string.format("%s %s", name, msg) or name
+          end
+
+          local sign = "" -- nf-fa-gear \uf013
+          local lsp_clients = vim.lsp.get_active_clients()
+          local messages_map = {}
+          for _, climsg in ipairs(client_messages) do
+            messages_map[climsg.name] = climsg.body
+          end
+
+          if #lsp_clients > 0 then
+            table.sort(lsp_clients, function(a, b)
+              return a.name < b.name
+            end)
+            local builder = {}
+            for _, cli in ipairs(lsp_clients) do
+              if
+                type(cli) == "table"
+                and type(cli.name) == "string"
+                and string.len(cli.name) > 0
+              then
+                if messages_map[cli.name] then
+                  table.insert(builder, stringify(cli.name, messages_map[cli.name]))
+                else
+                  -- table.insert(builder, stringify(cli.name))
+                  table.insert(builder, '')
+                end
+              end
+            end
+            if #builder > 0 then
+              -- return sign .. " " .. table.concat(builder, ", ")
+              return table.concat(builder, ", ")
+            end
+          end
+          return ""
+        end,
+      }
+    end,
   },
   { 'b0o/incline.nvim', -- floating filenames
     enabled = false,
@@ -1407,7 +1465,7 @@ return {
           "pyright",
           "html",
           "cssls",
-          "tsserver",
+          -- "tsserver",
           "eslint",
           "lua_ls",
           -- "autopep8",
