4a530bc2b78a0990f2f1f037b04f75fe4464ec21
supasorn <supasorn@gmail.com>
2025-09-22 23:13:21 +0700

    auto commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1605,6 +1605,38 @@ return {
       { "<leader>du", function() require("dapui").toggle() end,         desc = "toggle dapui" },
       { "<leader>de", function() require("dapui").eval() end,         desc = "toggle dapui" },
     },
+    init = function() 
+      local grp = vim.api.nvim_create_augroup("dap_colors", { clear = true })
+      vim.fn.sign_define('DapBreakpoint',          { text='', texthl='DapBreakpoint',          linehl='DapBreakpoint',          numhl='DapBreakpoint' })
+      vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint',          linehl='DapBreakpoint',          numhl='DapBreakpoint' })
+      vim.fn.sign_define('DapBreakpointRejected',  { text='', texthl='DapBreakpointRejected',  linehl='DapBreakpointRejected',  numhl='DapBreakpointRejected' })
+      vim.fn.sign_define('DapLogPoint',            { text='', texthl='DapLogPoint',            linehl='DapLogPoint',            numhl='DapLogPoint' })
+      vim.fn.sign_define('DapStopped',             { text='', texthl='DapStopped',             linehl='DapStoppedLine',         numhl='DapStopped' })
+
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = grp,
+        pattern = "*",
+        desc = "Set DAP marker colors and prevent theme from resetting them",
+        -- nested + schedule makes this run *after* theme's own ColorScheme logic
+        nested = true,
+        callback = function()
+          vim.schedule(function()
+            local sign_column_hl = vim.api.nvim_get_hl(0, { name = 'SignColumn' })
+            local sign_column_bg = (sign_column_hl.bg ~= nil) and ('#%06x'):format(sign_column_hl.bg) or 'bg'
+            local sign_column_ctermbg = (sign_column_hl.ctermbg ~= nil) and sign_column_hl.ctermbg or 'Black'
+            vim.api.nvim_set_hl(0, 'DapStopped',             { fg = '#00ff00', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapStoppedLine',         { bg = '#2e4d3d', ctermbg = 'Green' })
+            vim.api.nvim_set_hl(0, 'DapBreakpoint',          { fg = '#c23127', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapBreakpointRejected',  { fg = '#888ca6', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+            vim.api.nvim_set_hl(0, 'DapLogPoint',            { fg = '#61afef', bg = sign_column_bg, ctermbg = sign_column_ctermbg })
+          end)
+        end,
+      })
+
+      -- executing the settings explicitly for the first time
+      vim.api.nvim_exec_autocmds("ColorScheme", { group = "dap_colors" })
+    end,
     config = function()
       local dap = require("dap")
 
@@ -1643,7 +1675,7 @@ return {
               "stacks",
               "watches",
             },
-            size = 40, position = "left",
+            size = 40, position = "right",
           },
         },
         controls = {
