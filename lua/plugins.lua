e12ab8bc4abdc6c3ec9551e34a30ebdc9abd751c
supasorn <supasorn@gmail.com>
2026-01-05 12:57:56 +0700

    auto commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1536,9 +1536,11 @@ return {
       })
       vim.api.nvim_create_autocmd('FileType', {
         pattern = { '<filetype>' },
-        callback = function() vim.treesitter.start() end,
-        -- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        -- vim.wo[0][0].foldmethod = 'expr'
+        callback = function() 
+          vim.treesitter.start() 
+          vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.wo[0][0].foldmethod = 'expr'
+        end,
       })
     end,
   },
