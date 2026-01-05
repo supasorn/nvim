c96a498427494eb6aac2a9e0d2608a005b303b40
supasorn <supasorn@gmail.com>
2026-01-05 14:19:39 +0700

    auto commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -366,7 +366,23 @@ return {
     main = "ibl",
     ---@module "ibl"
     ---@type ibl.config
-    opts = {},
+    config = function(_, opts)
+      local highlight = {
+        "LineNr",
+        -- "IndentBlanklineChar",
+        "WinSeparator",
+      }
+
+      local hooks = require "ibl.hooks"
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "Indent1", { fg = "#707070" })
+        vim.api.nvim_set_hl(0, "Indent2", { fg = "#444444" })
+      end)
+
+      require("ibl").setup { indent = { highlight = highlight } }
+    end,
   },
   { 'nvim-treesitter/nvim-treesitter-context', -- Show context at the top. Cool!
     event = "VeryLazy",
