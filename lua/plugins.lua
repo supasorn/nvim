1dea36a86cc350670b0d732ad07ca7a8f5c1945d
supasorn <supasorn@gmail.com>
2026-01-06 19:24:38 +0700

    auto commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1510,7 +1510,7 @@ return {
             runtime = { version = "LuaJIT" },
             diagnostics = { globals = { "vim" } },
             workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
+              -- library = vim.api.nvim_get_runtime_file("", true),
               checkThirdParty = false,
             },
             completion = { callSnippet = "Replace" },
