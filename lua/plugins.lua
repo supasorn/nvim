c0ea86fa8d11bb30bc0bd7dd6f806402bd9dfd84
supasorn <supasorn@gmail.com>
2026-01-08 19:57:31 +0700

    auto commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2058,17 +2058,6 @@ return {
       server_opts_overrides = {},
     },
   },
-  { 'CopilotC-Nvim/CopilotChat.nvim',
-    enabled = false,
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-  },
   { 'olimorris/codecompanion.nvim',
     dependencies = {
       "nvim-lua/plenary.nvim",
